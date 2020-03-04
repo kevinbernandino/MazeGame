62e608628aa3e9bfdcd58170a0da945a546c43de
kevin_bernandino <kevinbernandino@gmail.com>
2020-03-04 13:32:47 -0800

    second commit

---
+++ maze.js
@@ -0,0 +1,105 @@
+function rand(max){
+    return Math.floor(Math.random()* max);
+}
+
+function shuffle(a){
+    for(let i = a.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+function changeBrightness(factor, sprite){
+    var virtCanvas = document.createElement("canvas");
+    virtCanvas.width = 500;
+    virtCanvas.height = 500;
+    var context = virtCanvas.getContext("2d");
+    context.drawImage(sprite, 0, 0, 500, 500);
+
+    var imgData = context.getImageData(0, 0, 500, 500);
+
+    for(let i = 0; i < imgData.data.length; i += 4){
+        imdData.data[i] = imgData.data[i] * factor;
+        imgData.data[i + 1] = imgData.data[i + 1] * factor;
+        imdData.data[i + 2] = imgData.data[i + 2]* factor;
+    }
+    context.putImageData(imgData, 0, 0);
+
+    var spriteOutput = new Image();
+    spriteOutput.src = virtCanvas.toDataURL();
+    virtCanvas.remove();
+    return spriteOutput;
+}
+
+function displayVictoryMess(moves){
+    document.getElementById("moves").innerHTML = "You Moved " + moves + " Steps.";
+    toggleVisablity("Message-Container");
+}
+
+function toggleVisablity(id){
+    if(document.getElementById(id).style.visibility == "visible"){
+        document.getElementById(id).style.visibility = "hidden";
+    } else{
+        document.getElementById(id).style.visibility = "visible";
+    }
+}
+
+function Maze(Width, Height) {
+    var mazeMap;
+    var width = Width;
+    var height = Height;
+    var startCoord, endCoord;
+    var dirs = ["n", "s", "e", "w"];
+    var modDir = {
+        n: {
+            y: -1,
+            x: 0,
+            o: "s"
+        },
+        s: {
+            y: 1,
+            x:0,
+            o: "n"
+        },
+        e: {
+            y:0,
+            x: 1,
+            o: "e"
+        },
+        w: {
+            y:0,
+            x: -1,
+            o: "e"
+        }
+    };
+
+this.map = function(){
+    return mazeMap;
+};
+this.startCoord = function(){
+    return startCoord;
+};
+this.endCoord = function(){
+    return endCoord;
+};
+
+function getMap(){
+    mazeMap = new Array(height);
+    for(y = 0; y < height; y++){
+        mazeMap[y] = new Array(width);
+        for(x = 0; x < width; ++x){
+            mazeMap[y][x] = {
+                n: false,
+                s: false,
+                e: false,
+                w: false,
+                visited: false,
+                priorPos: null
+            };
+        }
+    }
+}
+
+
+}
